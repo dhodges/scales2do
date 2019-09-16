89d5001acb5f3164b7b6a8791febc8c5d927227c
David Hodges <hodges.david@gmail.com>
2019-09-16 10:05:02 +1000

    tweak hiccup, use "#id" notation

--- src/scales2do/circle_component.cljs
+++ src/scales2do/circle_component.cljs
@@ -45,7 +45,7 @@
 
 (defn segment-lines []
   [:g
-   [:line {:id "line" :x1 200 :y1 200 :x2 148 :y2 15 :stroke-width 1}]
+   [:line#line {:x1 200 :y1 200 :x2 148 :y2 15 :stroke-width 1}]
    [:use {:xlinkHref "#line" :transform "rotate( 30,200,200)"}]
    [:use {:xlinkHref "#line" :transform "rotate( 60,200,200)"}]
    [:use {:xlinkHref "#line" :transform "rotate( 90,200,200)"}]
@@ -61,15 +61,13 @@
 (defn major-minor-labels []
   "curved labels 'Major' and 'Minor', marking the outer and inner circles"
   [:g {:dominant-baseline "baseline" :stroke "gray" :fill "gray"}
-   [:path {:id "major-path"
-           :d (describe-arc {:cx 200 :cy 200 :radius 132 :start-angle -10 :end-angle 12 :clockwise true})
-           :fill "transparent" :stroke "none"}]
+   [:path#major-path {:d (describe-arc {:cx 200 :cy 200 :radius 132 :start-angle -10 :end-angle 12 :clockwise true})
+                      :fill "transparent" :stroke "none"}]
    [:text {:class "label"}
     [:textPath {:xlinkHref "#major-path" :text-anchor "start"} "Major"]]
 
-   [:path {:id "minor-path"
-           :d (describe-arc {:cx 200 :cy 200 :radius 125 :start-angle -168 :end-angle -192 :clockwise false})
-           :fill "transparent" :stroke "none"}]
+   [:path#minor-path {:d (describe-arc {:cx 200 :cy 200 :radius 125 :start-angle -168 :end-angle -192 :clockwise false})
+                      :fill "transparent" :stroke "none"}]
    [:text {:class "label"}
     [:textPath {:xlinkHref "#minor-path" :text-anchor "start"} "Minor"]]])
 
@@ -88,10 +86,10 @@
 (defn circle-svg []
   [:svg {:width 400 :height 400}
    [:g {:stroke "gray" :fill "white"}
-    [:circle {:id "circle-outer"  :cx 200 :cy 200 :r 190 :stroke-width 3}]
-    [:circle {:id "circle-middle" :cx 200 :cy 200 :r 130 :stroke-width 2}]
+    [:circle#circle-outer  {:cx 200 :cy 200 :r 190 :stroke-width 3}]
+    [:circle#circle-middle {:cx 200 :cy 200 :r 130 :stroke-width 2}]
     [segment-lines]
-    [:circle {:id "circle-inner"  :cx 200 :cy 200 :r  70 :stroke-width 2}]
+    [:circle#circle-inner {:cx 200 :cy 200 :r  70 :stroke-width 2}]
     [major-minor-labels]
     [:g {:text-anchor "middle" :dominant-baseline "middle" :fill "black"}
      [major-scales]
